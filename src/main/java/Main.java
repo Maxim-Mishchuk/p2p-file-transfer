25198d3c79f197e048da7603bde81d64d19a4c73
Maksym Mishchuk <maxim.mishchuk@gmail.com>
2023-12-11 12:55:25 +0200

    first prototype of p2p file transfer

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,26 +3,25 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) {
         try {
-            P2PClient p2pClient = new P2PClient();
+            P2PFileTransfer p2PFileTransfer = new P2PFileTransfer();
             Thread serverThread = new Thread(() -> {
                 try {
-                    p2pClient.init();
+                    p2PFileTransfer.start();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
             });
-            Thread clientThread = new Thread(() -> {
-                try {
-                    p2pClient.send("hello", "192.168.1.139");
-                } catch (IOException e) {
-                    throw new RuntimeException(e);
-                }
-            });
+//            Thread clientThread = new Thread(() -> {
+//                try {
+//                    p2pClient.send("192.168.1.139", "hola".getBytes(StandardCharsets.UTF_8));
+//                } catch (IOException e) {
+//                    throw new RuntimeException(e);
+//                }
+//            });
 
             serverThread.start();
-            Thread.sleep(1000);
-            clientThread.start();
-        } catch (IOException | InterruptedException exception) {
+//            clientThread.start();
+        } catch (IOException exception) {
             exception.printStackTrace();
         }
     }
